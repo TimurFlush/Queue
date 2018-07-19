a66215e32f5e3b6a68a755b1fb23d767aa4420f8
Timur Flush <flush02@tutanota.com>
2018-07-19 15:13:50 +0500

    Added beanstalk installer for travis ci

---
+++ tests/_ci/install_beanstalk.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# trace ERR through pipes
+set -o pipefail
+
+# trace ERR through 'time command' and other functions
+set -o errtrace
+
+# set -u : exit the script if you try to use an uninitialised variable
+set -o nounset
+
+# set -e : exit the script if any statement returns a non-true return value
+set -o errexit
+
+sudo apt-get install -y beanstalkd
+
+TRAVIS_CONFIG=/etc/default/beanstalkd
+
+if [ ! -f "${TRAVIS_CONFIG}" ]; then
+  echo -e "The ${TRAVIS_CONFIG} file does not exists."
+  exit 1;
+fi
+
+exit 1;
